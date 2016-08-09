729d31bbc5703850bb7b0bbf8e1a57f7c78997bf
Erik Rose <erik@mozilla.com>
2016-08-09 17:55:15 -0400

    Add dispatch handler for flibbety jibbeting.

---
+++ Constructive Code Review/code.py
@@ -0,0 +1,24 @@
+def do_stuff(thing):
+    if thing == 2:
+        print "Flibbety jibbet!"
+        render_golfclubs()
+        sneeze_loudly(True)
+        do_other_stuff()
+
+    if thing == 4:
+        stir('A')
+        print "Flibbety jibbet!"
+        render_golfclubs()
+        sneeze_loudly(True)
+        do_other_stuff()
+
+    if thing == 5:
+        stir('B')
+        print "Flibbety jibbet!"
+        render_golfclubs()
+        sneeze_loudly(True)
+        do_other_stuff()
+
+    if thing == 7:
+        stir('C')
+        do_other_stuff()
